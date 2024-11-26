d2e26097518b2585f9c342de358bf918b3942ca2
NEJANX <matheeshanejan@gmail.com>
2024-11-26 17:02:47 +0530

    Include WebView

---
+++ app/src/main/java/me/nejan/pulseroom/MainActivity.java
@@ -0,0 +1,28 @@
+package me.nejan.pulseroom;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    WebView Firebase;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Firebase = findViewById(R.id.Firebase);
+        Firebase.getSettings().setJavaScriptEnabled(true);
+        Firebase.setWebViewClient(new WebViewClient());
+        Firebase.loadUrl("https://admin.members.rccsonline.com");
+    }
+}
